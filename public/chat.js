7b2213c281637c2167d5bbddf2d17d9015fae34e
Jamie Sookprasong <jfsmills@umich.edu>
2014-01-13 02:59:09 -0500

    Fixing Chat Bug
    push all of data to messages array, not just data.message!

--- public/chat.js
+++ public/chat.js
@@ -8,7 +8,7 @@ window.onload = function() {
 
 	socket.on('message', function(data) {
 		if(data.message) {
-			messages.push(data.message);
+			messages.push(data);
 			var html = '';
 			for(var i=0; i<messages.length; i++){
 				html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
