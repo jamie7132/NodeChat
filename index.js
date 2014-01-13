33a431ea8dacbcb841e66413400487011d22a1a7
Jamie Sookprasong <jfsmills@umich.edu>
2014-01-13 02:53:35 -0500

    Username
    think it's still broken..

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ var express = require('express');
 var app = express();
 var port = 3700;
 
-app.get("/", function(req, res){
-	res.send("It works!");
-});
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
 
 app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+	res.render("page");
+});
+
+
 var io = require('socket.io').listen(app.listen(port));
-console.log("Listening on port " + port);
 
 io.sockets.on('connection', function(socket){
 	socket.emit('message', { message: 'welcome to the chat'});
@@ -17,9 +22,4 @@ io.sockets.on('connection', function(socket){
 	});
 });
 
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-app.get("/", function(req, res){
-	res.render("page");
-});
+console.log("Listening on port " + port);
